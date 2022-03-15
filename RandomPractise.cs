cc3c905fab07613951d70640ca1fa33fcc50b704
zeynepvrl <97826119+zeynepvrl@users.noreply.github.com>
2022-03-15 13:18:55 +0300

    Add files via upload

---
+++ RandomPractise.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace supertoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Random x = new Random();
+            int sayı1, sayı2, sayı3, sayı4;
+            sayı1 = x.Next(1, 3);
+            sayı2 = x.Next(1, 3);
+            sayı3 = x.Next(1, 3);
+            sayı4 = x.Next(1, 3);
+            label1.Text = sayı1.ToString();
+            label2.Text = sayı2.ToString();
+            label3.Text = sayı3.ToString();
+            label4.Text = sayı4.ToString();
+            if(textBox1.Text==label1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+            if (textBox2.Text == label2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+            if (textBox3.Text == label3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+            if (textBox4.Text == label4.Text)
+            {
+                textBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }
+
+
+        }
+    }
+}
