e539df32c78d5d03ffcb8e32e193ac3b10f5a25c
zeynepvrl <97826119+zeynepvrl@users.noreply.github.com>
2022-03-15 13:01:50 +0300

    Add files via upload

---
+++ formlarArasıVeriTaşıma/formlarArasıVeriTaşıma/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formlarArasıVeriTaşıma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 zeyn = new Form2();
+            zeyn.kimden = textBox1.Text;
+            zeyn.mesaj = textBox2.Text;
+            zeyn.Show();
+            this.Hide();
+        }
+    }
+}
